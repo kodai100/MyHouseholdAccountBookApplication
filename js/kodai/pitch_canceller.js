f19d2b30f1594565c94e74ece551730aedee52ce
kodai_mac <kodai100@gmail.com>
2018-03-11 05:44:00 +0900

    Initial Commit.

---
+++ js/kodai/pitch_canceller.js
@@ -0,0 +1,14 @@
+/* ピッチインピッチアウトによる拡大縮小を禁止 */
+document.documentElement.addEventListener('touchstart', function (e) {
+    if (e.touches.length >= 2) {e.preventDefault();}
+}, false);
+
+/* ダブルタップによる拡大を禁止 */
+var t = 0;
+document.documentElement.addEventListener('touchend', function (e) {
+    var now = new Date().getTime();
+    if ((now - t) < 350){
+        e.preventDefault();
+    }
+    t = now;
+}, false);
---
+++ js/kodai/random_image.js
@@ -0,0 +1,15 @@
+var images = ["p1.png", "p2.png", "p3.png", "p4.png", "p5.png", "p6.png"];
+var img_root_path = "img/penguin/"; 
+
+change_img();
+
+// Must be included under img tag
+function change_img(){
+
+    var dom_img = document.getElementById("penguin");
+
+    var image = images[Math.floor(Math.random() * images.length)];
+    var path = img_root_path + image;
+
+    dom_img.src = path;
+}
