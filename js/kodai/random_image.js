980f49f779a20305643afeb2457fb44d5ea6ac9a
kodai_mac <kodai100@gmail.com>
2018-03-11 14:07:17 +0900

    画像追加

--- js/kodai/random_image.js
+++ js/kodai/random_image.js
@@ -1,4 +1,4 @@
-var images = ["p1.png", "p2.png", "p3.png", "p4.png", "p5.png", "p6.png"];
+var images = ["p1.png", "p2.png", "p3.png", "p4.png", "p5.png", "p6.png", "p7.gif"];
 var img_root_path = "img/penguin/"; 
 
 change_img();
